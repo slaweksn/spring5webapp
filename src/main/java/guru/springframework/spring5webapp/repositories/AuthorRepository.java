868f6201b943bc0a959bac59f4e469fb30c3bf24
joe <joe@minint>
2022-12-23 01:03:59 +0100

    commit (16. Spring Data Repositories) v2

--- src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -1,11 +1,11 @@
 package guru.springframework.spring5webapp.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import guru.springframework.spring5webapp.domain.Author;
 
 @Repository
-public interface AuthorRepository extends JpaRepository<Author, Long> {
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 
 }
--- src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -1,11 +1,11 @@
 package guru.springframework.spring5webapp.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import guru.springframework.spring5webapp.domain.Book;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends CrudRepository<Book, Long> {
 
 }
